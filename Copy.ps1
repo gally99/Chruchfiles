e4b252921d66dfaad976a93f42e9b801250a43c1
gally99 <58738006+gally99@users.noreply.github.com>
2021-03-07 19:57:21 -0500

    Create Copy.ps1

---
+++ Copy.ps1
@@ -0,0 +1,22 @@
+## copy the code from below location and run once. Its a function that will enable parallelism
+https://github.com/RamblingCookieMonster/Invoke-Parallel/blob/master/Invoke-Parallel/Invoke-Parallel.ps1
+
+
+### Actual you need to execute for parallel copy
+### Throttle limit - modify based on how many drives you want to copy at a time
+
+$Drivelist = @(
+  'a:\'
+  'b:\'
+  'c:\'
+  'd:\'
+)
+
+Invoke-Parallel -InputObject $Drivelist -ScriptBlock {
+
+   Copy-Item C:\ChrurchFiles $_ -Recurse
+  Write-verbose "$_" -Verbose
+
+} -Throttle 1000
+
+
